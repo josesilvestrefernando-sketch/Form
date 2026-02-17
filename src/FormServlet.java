f739535debb14e88053700135fb66252240558d5
josesilvestrefernando-sketch <josesilvestrefernando@gmail.com>
2026-02-17 20:26:35 +0800

    Add files via upload

--- src/FormServlet.java
+++ src/FormServlet.java
@@ -1,1 +1,24 @@
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+@WebServlet("/submitForm")
+public class FormServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name = request.getParameter("name");
+        String email = request.getParameter("email");
+        String message = request.getParameter("message");
+
+        // Process the form data (e.g., save to database, send an email, etc.)
+        System.out.println(name);
+        System.out.println(email);
+        System.out.println(message);
+        // For demonstration, you can redirect the user to a success page
+        response.sendRedirect("success.jsp");
+    }
+}
